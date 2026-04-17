a414b1ec45b72e8df366d9d58612371d8347386f
andreas-dahlen <David.Andreas.Dahlen@gmail.com>
2026-04-17 13:42:47 +0200

    more work on main and index.css

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -4,7 +4,7 @@ export default function MenuItem({ item }) {
 
   return (
     <div className="card-container" key={item.name}>
-      <h2 className="item-name">{item.name}</h2>
+      <h3 className="item-name">{item.name}</h3>
       <DietaryIcons tags={item.tags} />
       <p className="description"> {item.description}</p>
       <p className="price"> {item.price} SEK</p>
