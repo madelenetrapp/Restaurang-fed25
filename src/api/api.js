e1b50627bc13e5bc5797f0b65263092c467c626a
Madelene <madelenetrapp@outlook.com>
2026-04-17 14:59:29 +0200

    working on Api rendering

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,46 @@
-const response = await fetch('url?method=save', {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    key: "example-key",
-    value: "example value"
-  })
-});
-// If response.ok == true, the request succeeded
+const API_URL ='https://forverkliga.se/JavaScript/api/jsonStore.php'              //vi har ingen api? länken leder till ett exempel
+const KEY = 'menu'
 
-const response = await fetch('url?method=load&key=example-key', {
-  method: 'GET'
-});
-const data = await response.json();
-// If you use the wrong key, the API will return NULL
+async function loadMenu() {
+  const responce = await fetch(`${API_URL}?method=load&key=${KEY}`)
+  const data = await responce.json()
+  return data ?? []
+}
+export {loadMenu}
+
+async function saveMenu(menu) {
+  const response = await fetch(`${API_URL}?method=save`, {   //method=save beror på vad api säger!
+    method:'POST',                                          //detta beror på vad api säget; kan vara get, put osv!
+    headers: {
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      key:KEY,               //key beror på vad api säger!
+      value:menu             //value beror på vad api säger!
+    })
+  })
+  return response.ok
+}
+export {saveMenu}
+
+
+// Detta är ett exempel:
+// const response = await fetch('url?method=save', {
+//   method: 'POST',
+//   headers: {
+//     'Accept': 'application/json',
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     key: "example-key",
+//     value: "example value"
+//   })
+// });
+// // If response.ok == true, the request succeeded
+
+// const response = await fetch('url?method=load&key=example-key', {
+//   method: 'GET'
+// });
+// const data = await response.json();
+// // If you use the wrong key, the API will return NULL
+
