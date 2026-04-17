de2189347027b4d8ef5f342474c84f1b3dccae6a
andreas-dahlen <David.Andreas.Dahlen@gmail.com>
2026-04-17 19:03:55 +0200

    overLay layer for easier web flow

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,46 @@
 export default function Footer() {
+<<<<<<< Updated upstream
 
 
   return (
     <footer>i am footer!</footer>
   )
 }
+=======
+  const navLinkClass =
+    (base) =>
+      ({ isActive }) =>
+        isActive ? `${base} active` : base;
+
+  return (
+    <footer>
+      <div className='footer-background'></div>
+      {/* Logo */}
+      <img className="footer-logo" src={logo} alt="Lyan logo" />
+
+      {/* Address */}
+      <div className="footer-address">
+        <p>Sjöviksgatan 14B</p>
+        <p>417 56</p>
+        <p>Gothenburg</p>
+        <p>Sweden</p>
+      </div>
+
+      {/* Contact */}
+      <div className="footer-contact">
+        <p>Contact us:</p>
+        <p>070 123 45 67</p>
+        <p>lyan@gmail.com</p>
+      </div>
+
+      {/* Menu */}
+      <NavLink to="/menu" className={navLinkClass("navlink-button")}>
+        Menu
+      </NavLink>
+
+      {/* Admin */}
+      <span className="footer-admin">Admin</span>
+    </footer>
+  );
+}
+>>>>>>> Stashed changes
---
+++ src/components/Overlays.jsx
@@ -0,0 +1,9 @@
+export default function Overlays() {
+
+  return (
+    <>
+      <div className='header-img'></div>
+      <div className='footer-img'></div>
+    </>
+  )
+}
