7c7f542ba5f2d196af4fd28166adea8fc2d2a3d9
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-04-20 15:15:40 +0200

    Renamed an icon

--- src/components/DietaryIcons.jsx
+++ src/components/DietaryIcons.jsx
@@ -1,14 +1,18 @@
+import glutenFreeIcon from '../assets/icon-gluten-free.svg';
+import veganIcon from '../assets/icon-vegan.svg';
+import spicyIcon from '../assets/icon-spicy.svg';
+
 export default function DietaryIcons({ tags }) {
 
   return (
     <div className='dietary-icons'>
       {tags.glutenFree &&
-        <img src='./src/assets/icon-gluten-free.svg' alt="Gluten free"></img>}
+        <img src={glutenFreeIcon} alt="Gluten free"></img>}
 
-      {tags.vegan && <img src='./src/assets/icon-vegan.svg' alt="Vegan"></img>}
+      {tags.vegan && <img src={veganIcon} alt="Vegan"></img>}
 
       {[...Array(tags.spicy || 0)].map((_, i) => (
-        <img key={i} src='./src/assets/icon-spicy.svg' alt="Chili icon" />
+        <img key={i} src={spicyIcon} alt="Chili icon" />
       ))}
     </div>
   )
