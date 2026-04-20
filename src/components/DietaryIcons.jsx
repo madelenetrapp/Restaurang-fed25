9f95b86b77f75128713e6b95ee75d5f41bc54c44
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-04-20 14:48:27 +0200

    Added gluten-free and spicy icons

--- src/components/DietaryIcons.jsx
+++ src/components/DietaryIcons.jsx
@@ -3,12 +3,12 @@ export default function DietaryIcons({ tags }) {
   return (
     <div className='dietary-icons'>
       {tags.glutenFree &&
-        <img src={null} alt="Gluten free"></img>}
+        <img src='./src/assets/icon-gluten-free.svg' alt="Gluten free"></img>}
 
       {tags.vegan && <img src='./src/assets/icon-vegan.svg' alt="Vegan"></img>}
 
       {[...Array(tags.spicy || 0)].map((_, i) => (
-        <img key={i} src={null} alt="Chili icon" />
+        <img key={i} src='./src/assets/icon-spicy.svg' alt="Chili icon" />
       ))}
     </div>
   )
