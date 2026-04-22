9a18059c8e7c0ff2dd72363b413948735e5991fe
Madelene <madelenetrapp@outlook.com>
2026-04-22 17:39:18 +0200

    burger img

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -11,6 +11,7 @@ export default function MenuItem({ item }) {
 
   return (
     <div className="card-container" key={item.name} onClick={add}>
+      
       <div className='item-name-and-icons'>
         <DietaryIcons tags={item.tags} />
         <h3 className="card-header">{item.name}</h3>
--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -2,6 +2,9 @@ import MenuItem from '../components/MenuItem.jsx'
 import DebugOverlay from '../debug/DebugOverlay.jsx';
 import { useLoaderData } from 'react-router';
 import { useTypeSort } from '../hooks/useTypeSort.js';
+import burger1 from '../assets/burger1.svg'
+import burger2 from '../assets/burger2.svg'
+import burger3 from '../assets/burger3.svg'
 
 export default function MenuPage() {
 
@@ -11,19 +14,37 @@ export default function MenuPage() {
   return (
 
     <div className='menu-background'>
+
+      <div className='menu-layout-wrapper1'>
+        <img src={burger1} className="burger left" />
+      </div>
+
+
       <DebugOverlay />
       {/* TODO remove debugOverlay in prod! */}
 
+      {/* NY importerar bilder placering i css */}
       <h1>Menu</h1>
+
 
       <div className='grid-box'>
         {menuTypes.map(type => (
           <div key={type} className='type-box' >
 
+
             <h2> {type} </h2>
             {menu.filter(s => s.type === type).map(item => (
               <MenuItem key={item.name} item={item} />
             ))}
+
+
+            {/* <div className='menu-layout-wrapper2'>
+              <img src={burger2} className="burger center" />
+            </div>
+            <div className='menu-layout-wrapper2'>
+              <img src={burger3} className="burger right" />
+            </div> */}
+
           </div>
         ))}
       </div>
