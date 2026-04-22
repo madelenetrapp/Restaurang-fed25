5bfaa068b44e96cc82552dc324d8f16ccc463649
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2026-04-22 11:53:18 +0200

    Updated dietary icons styles

--- src/components/DietaryIcons.jsx
+++ src/components/DietaryIcons.jsx
@@ -5,17 +5,28 @@ import spicyIcon from '../assets/icon-spicy.svg';
 export default function DietaryIcons({ tags }) {
 
   return (
-    <div className='dietary-icons'>
-      {tags.glutenFree &&
-        <img src={glutenFreeIcon} alt="Gluten free" />}
+		<div className="dietary-icons">
+			{tags.glutenFree && (
+				<img
+					src={glutenFreeIcon}
+					alt="Gluten free"
+					className="icon-gluten-free"
+				/>
+			)}
 
-      {tags.vegan && <img src={veganIcon} alt="Vegan" />}
+			{tags.vegan && (
+				<img src={veganIcon} alt="Vegan" className="icon-vegan" />
+			)}
 
-
-      {/* TODO needs to be reworked and simplified */}
-      {[...Array(tags.spicy || 0)].map((_, i) => (
-        <img key={i} src={spicyIcon} alt="Chili icon" />
-      ))}
-    </div>
-  )
+			{/* TODO needs to be reworked and simplified */}
+			{[...Array(tags.spicy || 0)].map((_, i) => (
+				<img
+					key={i}
+					src={spicyIcon}
+					alt="Chili icon"
+					className="icon-spicy"
+				/>
+			))}
+		</div>
+  );
 }
