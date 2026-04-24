3d051e92ad5069ec210c8ed3744ffa27cc84ffd6
andreas-dahlen <David.Andreas.Dahlen@gmail.com>
2026-04-24 16:58:50 +0200

    admin - edit item should be done!

--- src/components/menu/MenuLayout.jsx
+++ src/components/menu/MenuLayout.jsx
@@ -3,8 +3,8 @@ import { useMenuStore } from '../../hooks/useMenuStore';
 import { useTypeSort } from '../../hooks/useTypeSort';
 import burgerTall from '../../assets/burger-tall.png'
 import { useLoaderData } from 'react-router';
-import MenuItem from './MenuItem'
-import AdminMenuItem from './AdminMenuItem'
+import UserMenu from './UserMenu'
+import AdminMenu from './AdminMenu'
 import { NavLink } from 'react-router';
 
 export default function MenuLayout({ isAdmin }) {
@@ -37,14 +37,14 @@ export default function MenuLayout({ isAdmin }) {
               {isAdmin
                 ? menu.filter((s) => s.type === type)
                   .map((item) => (
-                    <AdminMenuItem
+                    <AdminMenu
                       key={item.name}
                       item={item}
                     />
                   ))
 
                 : menu.filter(s => s.type === type).map(item => (
-                  <MenuItem key={item.name} item={item} />
+                  <UserMenu key={item.name} item={item} />
                 ))}
             </div>
           ))}
