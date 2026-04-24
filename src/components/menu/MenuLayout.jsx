8bc854905ae713a882aae40556d29de10e89c787
Madelene <madelenetrapp@outlook.com>
2026-04-24 17:19:05 +0200

    new pictures and changed position

--- src/components/menu/MenuLayout.jsx
+++ src/components/menu/MenuLayout.jsx
@@ -1,7 +1,8 @@
 import { useAuthStore } from '../../hooks/useAuthStore';
 import { useMenuStore } from '../../hooks/useMenuStore';
 import { useTypeSort } from '../../hooks/useTypeSort';
-import burgerTall from '../../assets/burger-tall.png'
+import burgerTallRight from '../../assets/burger-tall.png'
+import burgerTallLeft from '../../assets/burger-tall.png'
 // import { useLoaderData } from 'react-router';
 import MenuItem from './MenuItem'
 import AdminMenuItem from './AdminMenuItem'
@@ -22,34 +23,38 @@ export default function MenuLayout({ isAdmin }) {
         : ''}
 
 
-      <img src={burgerTall} alt="" />
+      <img src={burgerTallRight} alt="" className="burger-tall-right" />
+      <img src={burgerTallLeft} alt="" className="burger-tall-left" />
       <div className="menu-background">
+{/* 
+      <img src={burgerTall} alt="" />
+      <div className="menu-background"> */}
 
-        <h1>{isAdmin && "Admin "}Menu</h1>
+      <h1>{isAdmin && "Admin "}Menu</h1>
 
-        <div className='grid-box'>
-          {menuTypes.map(type => (
-            <div key={type} className='type-box' >
+      <div className='grid-box'>
+        {menuTypes.map(type => (
+          <div key={type} className='type-box' >
 
-              <h2> {type} </h2>
+            <h2> {type} </h2>
 
 
-              {isAdmin
-                ? menu.filter((s) => s.type === type)
-                  .map((item) => (
-                    <AdminMenuItem
-                      key={item.name}
-                      item={item}
-                    />
-                  ))
+            {isAdmin
+              ? menu.filter((s) => s.type === type)
+                .map((item) => (
+                  <AdminMenuItem
+                    key={item.name}
+                    item={item}
+                  />
+                ))
 
-                : menu.filter(s => s.type === type).map(item => (
-                  <MenuItem key={item.name} item={item} />
-                ))}
-            </div>
-          ))}
-        </div>
+              : menu.filter(s => s.type === type).map(item => (
+                <MenuItem key={item.name} item={item} />
+              ))}
+          </div>
+        ))}
       </div>
+    </div >
     </>
   )
 }
