d108f08b8924f0dbb60a978a3d7c14fe4372188f
Valentino Finocchiaro <Valentino.finocchiaro@student.nbi-handelsakademin.se>
2026-04-27 13:28:45 +0200

    resovled merge conflicts

--- src/components/cart/PriceDisplay.jsx
+++ src/components/cart/PriceDisplay.jsx
@@ -2,29 +2,6 @@ import { NavLink } from 'react-router'
 
 export default function PriceDisplay({ totalPrice, cart, handlePayment }) {
 
-<<<<<<< HEAD
-  return (
-    cart.length !== 0
-      ? (
-        <div className='price-display'>
-          <p className='total-text'>TOTAL: {totalPrice} SEK</p>
-          <div className='pay-btn-wrapper'>
-          <button className='button' onClick={handlePayment}> Place Order </button>
-          </div>
-        </div>
-          
-      )
-      : (
-        <div className='price-display'>
-          <h2> Your cart is empty</h2>
-          <h3> Add something delicious to get started </h3>
-          <div className='pay-btn-wrapper'>
-          <NavLink to='/menu' className='button'> View Menu</NavLink>
-          </div>
-        </div>
-      )
-  )
-=======
 	return cart.length !== 0 ? (
 		<div className="price-display">
 			<p className="total-text">TOTAL: {totalPrice} SEK</p>
@@ -44,5 +21,4 @@ export default function PriceDisplay({ totalPrice, cart, handlePayment }) {
 			</NavLink>
 		</div>
 	);
->>>>>>> dev
 }
